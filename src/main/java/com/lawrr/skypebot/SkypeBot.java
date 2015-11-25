8325eeb5814d4639ffc822c2a57fb441183eef60
Lawrence Ling <Lawrence1995@hotmail.com>
2015-11-26 04:24:59 +1100

    SkypeBot class refactor and added captcha solve method

--- src/main/java/com/lawrr/skypebot/SkypeBot.java
+++ src/main/java/com/lawrr/skypebot/SkypeBot.java
@@ -1,12 +1,15 @@
 package com.lawrr.skypebot;
 
+import com.lawrr.skypebot.modules.AsciiModule;
 import com.lawrr.skypebot.modules.YoutubeModule;
 import in.kyle.ezskypeezlife.EzSkype;
 import in.kyle.ezskypeezlife.api.SkypeCredentials;
+import in.kyle.ezskypeezlife.api.captcha.SkypeCaptcha;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class SkypeBot {
 
@@ -21,10 +24,9 @@ public class SkypeBot {
     }
 
     private void init() {
-        SkypeBotCredentials credentials;
         try {
-            // Get login credentials
-            credentials = EzSkype.GSON.fromJson(new FileReader(new File("credentials.json")), SkypeBotCredentials.class);
+            // Get credentials
+            SkypeBotCredentials credentials = EzSkype.GSON.fromJson(new FileReader(new File("credentials.json")), SkypeBotCredentials.class);
 
             // Create EzSkype instance
             ezSkype = new EzSkype(new SkypeCredentials(credentials.getUsername(), credentials.getPassword()));
@@ -32,18 +34,30 @@ public class SkypeBot {
 
             // Create modules
             YoutubeModule youtubeModule = new YoutubeModule(credentials.getYoutubeApiKey());
+            AsciiModule asciiModule = new AsciiModule();
 
             // Register events
+            ezSkype.getEventManager().registerEvents(this);
             ezSkype.getEventManager().registerEvents(youtubeModule);
+            ezSkype.getEventManager().registerEvents(asciiModule);
 
             // Start
             System.out.println("Bot started");
         } catch (FileNotFoundException e) {
-            System.out.println("File not found");
+            System.out.println("Credentials not found");
             System.exit(1);
         } catch (Exception e) {
             System.out.println("Error occurred");
             System.exit(1);
         }
+    }
+
+    // Called when a captcha needs to be solved
+    // Returns the solution to the captcha
+    @Override
+    public String solve(SkypeCaptcha skypeCaptcha) {
+        System.out.println("Enter the solution to " + skypeCaptcha.getUrl() + " then click enter");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
