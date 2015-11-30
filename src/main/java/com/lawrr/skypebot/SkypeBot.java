9efb7434a15bfe100b4fabb24455c5ef7a04bd25
Lawrence Ling <Lawrence1995@hotmail.com>
2015-11-30 16:23:29 +1100

    Added HelpModule

--- src/main/java/com/lawrr/skypebot/SkypeBot.java
+++ src/main/java/com/lawrr/skypebot/SkypeBot.java
@@ -1,8 +1,6 @@
 package com.lawrr.skypebot;
 
-import com.lawrr.skypebot.modules.AsciiModule;
-import com.lawrr.skypebot.modules.RichTextModule;
-import com.lawrr.skypebot.modules.YoutubeModule;
+import com.lawrr.skypebot.modules.*;
 import in.kyle.ezskypeezlife.EzSkype;
 import in.kyle.ezskypeezlife.api.SkypeCredentials;
 import in.kyle.ezskypeezlife.api.captcha.SkypeCaptcha;
@@ -11,6 +9,8 @@ import in.kyle.ezskypeezlife.api.captcha.SkypeErrorHandler;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SkypeBot implements SkypeErrorHandler {
@@ -38,12 +38,22 @@ public class SkypeBot implements SkypeErrorHandler {
             YoutubeModule youtubeModule = new YoutubeModule(credentials.getYoutubeApiKey());
             AsciiModule asciiModule = new AsciiModule();
             RichTextModule richTextModule = new RichTextModule(ezSkype.getLocalUser().getUsername());
+            HelpModule helpModule = new HelpModule();
+
+            // Set modules
+            List<Module> modules = new ArrayList<Module>();
+            modules.add(youtubeModule);
+            modules.add(asciiModule);
+            modules.add(richTextModule);
+            modules.add(helpModule);
+            helpModule.setModules(modules);
 
             // Register events
             ezSkype.getEventManager().registerEvents(this);
             ezSkype.getEventManager().registerEvents(youtubeModule);
             ezSkype.getEventManager().registerEvents(asciiModule);
             ezSkype.getEventManager().registerEvents(richTextModule);
+            ezSkype.getEventManager().registerEvents(helpModule);
 
             // Start
             System.out.println("Bot started");
---
+++ src/main/java/com/lawrr/skypebot/modules/HelpModule.java
@@ -0,0 +1,69 @@
+package com.lawrr.skypebot.modules;
+
+import com.google.api.client.util.Joiner;
+import com.lawrr.skypebot.CommandParser;
+import in.kyle.ezskypeezlife.events.conversation.SkypeMessageReceivedEvent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HelpModule implements Module {
+
+    public List<String> commands = new ArrayList<>(
+            Arrays.asList(
+                    "!help"
+            )
+    );
+    private List<Module> modules;
+
+    public HelpModule() {
+        modules = new ArrayList<Module>();
+    }
+
+    public HelpModule(List<Module> modules) {
+        this.modules = modules;
+    }
+
+    public List<String> getCommands() {
+        return commands;
+    }
+
+    public void setModules(List<Module> modules) {
+        this.modules = modules;
+    }
+
+    public void handleCommands(SkypeMessageReceivedEvent e) {
+        String message = e.getMessage().getMessage();
+        List<String> command = CommandParser.parse(message);
+
+        // Check if possible command
+        if (command.size() > 0) {
+            String replyMessage = "";
+            switch (command.get(0)) {
+                // Help
+                case "!help": {
+                    List<String> allCommands = new ArrayList<String>();
+                    for (Module m : modules) {
+                        for (String s : m.getCommands()) {
+                            allCommands.add(s);
+                        }
+                    }
+                    replyMessage = Joiner.on('\n').join(allCommands);
+                    break;
+                }
+
+                // Other
+                default: {
+                    break;
+                }
+
+            }
+
+            if (!replyMessage.equals("")) {
+                e.reply(replyMessage);
+            }
+        }
+    }
+
+}
